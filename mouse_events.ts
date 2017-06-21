2a34ca11689984885183720cbf4d87d2126b032d
Kumudu Gunasekara <kumudug1@gmail.com>
2017-06-22 08:12:49 +1000

    map and get only x and y values

--- mouse_events.ts
+++ mouse_events.ts
@@ -2,7 +2,14 @@ import {Observable} from "rxjs/Observable";
 import 'rxjs/add/observable/fromEvent';
 
 
-let source = Observable.fromEvent(document, "mousemove");
+let source = Observable.fromEvent(document, "mousemove")
+                        .map((e : MouseEvent) => {
+                            return {
+                                x: e.clientX,
+                                y: e.clientY
+                            }
+                        });
+
 
 source.subscribe(
     value => console.log(value),
