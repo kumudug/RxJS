2ef88cbf2b439321282920e0470072c213772e10
Kumudu Gunasekara <kumudug1@gmail.com>
2017-07-24 07:43:59 +1000

    05 sending requests with xml http request (#5)
    * processing the xhr without rxjs
    
    * XML Http request

--- mouse_events.ts
+++ mouse_events.ts
@@ -5,6 +5,8 @@ import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/delay';
 
 let circle = document.getElementById("circle");
+let button = document.getElementById("button");
+let output = document.getElementById("output");
 
 let source = Observable.fromEvent(document, "mousemove")
                         .map((e : MouseEvent) => {
@@ -16,9 +18,24 @@ let source = Observable.fromEvent(document, "mousemove")
                         .filter(value => value.x < 500)
                         .delay(300);
 
+let click = Observable.fromEvent(button, "click");
+
 function onNext(value){
     circle.style.left = value.x;
     circle.style.top = value.y;
+}
+
+function load(url: string){
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("load", () => {
+        let movies = JSON.parse(xhr.responseText);
+        movies.forEach(m => {
+            let div = document.createElement("div");
+            div.innerText = m.title;
+            output.appendChild(div);
+        });
+    });
 }
 
 source.subscribe(
@@ -26,4 +43,10 @@ source.subscribe(
     onNext,
     e => console.log(`error: ${e}`),
     () => console.log("Complete")
+);
+
+click.subscribe(
+    e => load("movies.json"),
+    e => console.log(`error: ${e}`),
+    () => console.log("complete")
 );
